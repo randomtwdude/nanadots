8b6ba20ecea292be2cb6cf661accfbf9831b6a52
randomtwdude <105645765+randomtwdude@users.noreply.github.com>
2022-05-18 19:53:56 +0800

    Update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,6 +10,5 @@ polybar -q workspaceA >>/tmp/polybarA.log 2>&1 &
 polybar -q monitorB >>/tmp/polybarB.log 2>&1 &
 polybar -q music >>/tmp/polybarC.log 2>&1 &
 polybar -q internet1F >>/tmp/polybarD.log 2>&1 &
-polybar -q internet2G >>/tmp/polybarE.log 2>&1 &
 polybar -q audioH >>/tmp/polybarF.log 2>&1 &
 polybar -q datetimeI >>/tmp/polybarG.log 2>&1 &
